f3504eb6c51524098da84446bab95b78e2623690
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-03 17:34:26 +0100

    Add js human logic

---
+++ script.js
@@ -0,0 +1,11 @@
+// il bottone vine cliccato
+
+// viene generato un numero randomico da 1 a 90
+// non puo essere generato un numero gia estratto
+// il numero estratto camia colore sul tabellone
+// Se ci sono 2 numeri vicini è ambo
+// Se ci sono 3 numeri vicini è terno
+// Se ci sono 4 numeri vicini è quaterno
+// Se ci sono 5 numeri vicini è cinquina
+// Se ci sono 15 numeri vicini è tombola
+// Al suo completamento genera messaggio di congratulazioni

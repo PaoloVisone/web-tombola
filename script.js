4d4bb7f76dda65ff73bed18c896088211d382c35
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-03 20:37:04 +0100

    Add function for the extraction

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 // il bottone vine cliccato
+let button = document.querySelector('#btn-ext')
+let numberExt = document.querySelector('#number-ext')
+let tabelNumb = document.querySelector('td')
+
+
+button.addEventListener("click", pushNumber);
+
+// Comincia l'estrazione
+
+const numberExtract = [];
+
+console.log(numberExtract);
 
 // viene generato un numero randomico da 1 a 90
+
+function pushNumber() {
+
+    button = parseInt(Math.random() * (90) + 1);
+
+    numberExt.innerText = button;
+
+    numberExtract.push(button);
+
+}
+
 // non puo essere generato un numero gia estratto
-// il numero estratto camia colore sul tabellone
+// il numero estratto cambia colore sul tabellone
+// function changeColor() {
+//     tabelNumb.style.color = "red";
+// }
 // Se ci sono 2 numeri vicini è ambo
 // Se ci sono 3 numeri vicini è terno
 // Se ci sono 4 numeri vicini è quaterno
